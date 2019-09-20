4baeaac9c3c69613e67676a17954a1ba15ea1481
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-09-20 08:25:14 -0700

    Tested the objects with console.log.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -39,20 +39,21 @@ const vRaptor = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tRex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(vRaptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(steg.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tRex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+tRex.roar = () => "RAWERSRARARWERSARARARRRR!";
+console.log(tRex.roar);
 
 
 // ==== Arrays ====
