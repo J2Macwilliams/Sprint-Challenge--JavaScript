b38ecc647db72f3cd511a19b032baaefb9680f43
Jeremy McWilliams <j2macwilliams@gmail.com>
2019-09-20 08:53:02 -0700

    Finished Request 2 in the Advanced Array Methods section.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -146,6 +146,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map(function(item){
+    return lowCaseAnimalNames.push(item.animal_name.toLowerCase());
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
